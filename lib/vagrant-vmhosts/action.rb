ccb215b08a6bc40e29e4d61911ccfab11efff1f1
Kristof Willaert <kristof.willaert@gmail.com>
2012-05-24 14:06:43 +0200

    Handle the case where you don't specify any entries.

--- lib/vagrant-vmhosts/action.rb
+++ lib/vagrant-vmhosts/action.rb
@@ -8,13 +8,15 @@ module VagrantVmHosts
       @app.call(env)
 
       hosts = env[:vm].config.vmhosts.list
-      env[:ui].info I18n.t("vagrant.plugins.vmhosts.adding")
-      hosts.each do |host|
-        if !env[:vm].channel.test("grep '^#{host['ip']}[[:space:]]' /etc/hosts")
-          env[:vm].channel.sudo("printf '%s\t%s %s\n' '#{host['ip']}' '#{host['canonical']}' '#{host['aliases'].join(' ')}' >> /etc/hosts")
+
+      if !hosts.empty?
+        env[:ui].info I18n.t("vagrant.plugins.vmhosts.adding")
+        hosts.each do |host|
+          if !env[:vm].channel.test("grep '^#{host['ip']}[[:space:]]' /etc/hosts")
+            env[:vm].channel.sudo("printf '%s\t%s %s\n' '#{host['ip']}' '#{host['canonical']}' '#{host['aliases'].join(' ')}' >> /etc/hosts")
+          end
         end
       end
     end
   end
 end
-
--- lib/vagrant-vmhosts/config.rb
+++ lib/vagrant-vmhosts/config.rb
@@ -1,6 +1,10 @@
 module VagrantVmHosts
   class Config < Vagrant::Config::Base
-    attr_accessor :list
+    attr_writer :list
+
+    def list
+      @list || []
+    end
 
     def validate(env, errors)
       # check hash format
